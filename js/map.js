8be70ab88da5df532268f190f6ee2894d88d89f6
svmax18 <42927535+svmax18@users.noreply.github.com>
2018-09-17 01:28:42 +0300

    Update map.js

--- js/map.js
+++ js/map.js
@@ -24,28 +24,28 @@ var listAdd = generateAdd();
 addList(listAdd[0]);
 insertPin();
 
- // Шаблон объявления
-function addList(ad) {
-  var offer = ad.offer;
-  var author = ad.author;
-  var offerWindow = document.querySelector('.card');
+// Шаблон объявления
+function addList() {
+  var offer;
+  var author;
+  var offerWindow = document.querySelector('#card');
   var windowText = offerWindow.querySelector('.map__card popup');
   var windowAvatar = offerWindow.querySelector('.popup__avatar');
-  var addList = windowText.cloneNode(true);
-  windowAvatar.src = author.avatar
-  addList.querySelector('.popup__title').textContent = offer.title;
-  addList.querySelector('.popup__address').textContent = offer.adress;
-  addList.querySelector('.popup__price').innerHTML = getPrice(offer.price);
-  addList.querySelector('.popup__type').textContent = rusType(offer.type);
-  addList.querySelector('.popup__rooms-and-guests').textContent = getGuestsRooms(offer.guests, offer.rooms);
-  addList.querySelector('.popup__checkin-time').textContent = getTime(offer.checkin, offer.checkout);
-  addList.querySelector('.popup__features').appendChild = offer.features;
-  addList.querySelector('.popup__description').textContent = offer.description;
-  addList.querySelector('.popup__photos').appendChild = offer.photos;
-  offerWindow.appendChild(addList);
+  var addLists = windowText.cloneNode(true);
+  windowAvatar.src = author.avatar;
+  addLists.querySelector('.popup__title').textContent = offer.title;
+  addLists.querySelector('.popup__address').textContent = offer.adress;
+  addLists.querySelector('.popup__price').innerHTML = getPrice(offer.price);
+  addLists.querySelector('.popup__type').textContent = rusType(offer.type);
+  addLists.querySelector('.popup__rooms-and-guests').textContent = getGuestsRooms(offer.guests, offer.rooms);
+  addLists.querySelector('.popup__checkin-time').textContent = getTime(offer.checkin, offer.checkout);
+  addLists.querySelector('.popup__features').appendChild = offer.features;
+  addLists.querySelector('.popup__description').textContent = offer.description;
+  addLists.querySelector('.popup__photos').appendChild = offer.photos;
+  offerWindow.appendChild(addLists);
 }
 
-// генерация JS объектов с all
+// генерация JS объектов
 function generateAdd() {
   var add = [];
   for (var i = 0; i < users; i++) {
@@ -77,33 +77,32 @@ function generateAdd() {
   return add;
 }
 
-var avatars = function () {
+var avatars = function () {  //генерирует авки
   var arhiveAvatars = [];
-  for (var i = 1; i < users; i++) {
+  for (var i = 1; i <= users; i++) {
     i = '0' + i;
-    avatars  = 'img/avatars/user' + i + '.png';
+    avatars = 'img/avatars/user' + i + '.png';
     arhiveAvatars.push(avatars);
   }
   return arhiveAvatars;
 };
-	
 // Отрисовка на странице маркера
 function insertPin() {
-  var mapPin= document.querySelector('.map__pins');
-  var mapPin2 = document.createDocumentFragment();
+  var mapPin = document.querySelector('.map__pins');
+  var fragment = document.createDocumentFragment();
   for (var i = 0; i < listAdd.length; i++) {
-    mapPin2.appendChild(createPin(listAdd[i]));
+    fragment.appendChild(createPin(listAdd[i]));
   }
-  mapPin.appendChild(MapPin2);
+  mapPin.appendChild(fragment);
 }
 
-	// Маркер
+// Маркер
 function createPin(marker) {
   var userLocation = document.createElement('div');
   var userAvatar = document.createElement('img');
   userLocation.className = 'map__pins';
-  userLocation.style.left = (locationX - markerHeight) + 'px';
-  userLocation.style.top = locationY - (markerWidth / 2) + 'px';
+  userLocation.style.left = (location.x - markerHeight) + 'px';
+  userLocation.style.top = location.y - (markerWidth / 2) + 'px';
   userAvatar.className = 'popup__avatar';
   userAvatar.width = 70;
   userAvatar.height = 70;
@@ -115,7 +114,6 @@ function createPin(marker) {
 function getPrice(price) {
   return price + ' ₽/ночь';
 }
-	
 function rusType(type) {
   switch (type) {
     case 'flat':
@@ -127,30 +125,30 @@ function rusType(type) {
     case 'palace':
       return 'Дворец';
   }
+  return type;
 }
-	
+
 function getGuestsRooms(guests, rooms) {
   return rooms + ' комнаты для ' + guests + ' гостей';
 }
-	
+
 function getTime(checkin, checkout) {
   return 'Заезд после ' + checkin + ', выезд до ' + checkout;
 }
-	
+
 function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.round(Math.random() * (max - min)) + min;
 }
 
 function getRandomItem(array) {
-  for (var i = 0; i < array.length; i++) {
-    var randomList = Math.floor(Math.random() * array.length);
-  }
+  var randomList = Math.floor(Math.random() * array.length);
   var randomItem = array[randomList];
   return randomItem;
 }
 
-function getArrayLength() {
-  var newFeatures = service.slice();
-  newFeatures.length = getRandomNumber(service.length);
+function getArrayLength(array) {
+  var newFeatures = array.slice();
+  newFeatures.length = getRandomNumber(0, array.length);
   return newFeatures;
 }
+
